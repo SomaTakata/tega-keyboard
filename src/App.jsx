de8d4271adb771eafd54502ab91b34d5cd59f504
SomaTakata <hayatetaka123@gmail.com>
2022-12-18 16:48:50 +0900

    stateの使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,207 @@
-// import "./App.css";
-// import { fabric } from "fabric";
-// import { useEffect } from "react";
-// import { useState } from "react";
-
-// const App = () => {
-//   const setBrush = (canvas) => {
-//     if (canvas.freeDrawingBrush) {
-//       const brush = canvas.freeDrawingBrush;
-//       brush.color = "black";
-//       brush.width = 6;
-//     }
-//   };
-
-//   const setBackgroundImage = (canvas) => {
-//     "./canvas.png",
-//       (img) => {
-//         img.set({
-//           opacity: 1, // 透明度
-//           scaleX: canvas.width / img.width, // X軸の拡大率
-//           scaleY: canvas.height / img.height, // Y軸の拡大率
-//         });
-//         canvas.setBackgroundImage(img, canvas.requestRenderAll.bind(canvas)); // 画像を背景に設定
-//       };
-//   };
-
-//   // });
-//   useEffect(() => {
-//     const canvas = new fabric.Canvas("canvas", {
-//       isDrawingMode: true,
-//       height: 200,
-//       width: 200,
-//     });
-//     setBrush(canvas);
-//     setBackgroundImage(canvas);
-//   }, []);
-
-//   const download = () => {
-//     const canvas = document.getElementById("canvas");
-//     const link = document.getElementById("hiddenLink");
-
-//     link.href = canvas.toDataURL("image/png");
-//     localStorage.setItem("あ", link.href);
-//   };
-
-//   const clearCanvas = (e) => {
-//     const cvs = document.getElementById("canvas");
-//     const ctx = cvs.getContext("2d");
-//     ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-//     ctx.beginPath();
-
-//     console.log("リセット！");
-//   };
-
-//   return (
-//     <div id="items" align="center">
-//       <div id="canvasDiv">
-//         <canvas id="canvas"></canvas>
-//       </div>
-//       <div>
-//         <button onClick={clearCanvas}>クリア</button>
-//         <button onClick={download}>"あ"の追加</button>
-//         <a id="hiddenLink" download="canvas.png">
-//           link
-//         </a>
-//         <div id="next" align="center">
-//           <button id="a">＜＜　あ　</button>
-//           <button id="a">　い　＞＞</button>
-//         </div>
-//         <div id="gal">
-//           <img id="galI" src={localStorage.getItem("あ")}></img>
-//           <p id="galT">あ</p>
-//         </div>
-//         <div id="gal">
-//           <img id="galI" src={localStorage.getItem("い")}></img>
-//           <p id="galT">い</p>
-//         </div>
-//         <div id="gal">
-//           <img id="galI" src={localStorage.getItem("う")}></img>
-//           <p id="galT">う</p>
-//         </div>
-//         <div id="gal">
-//           <img id="galI" src={localStorage.getItem("え")}></img>
-//           <p id="galT">え</p>
-//         </div>
-//         <div id="gal">
-//           <img id="galI" src={localStorage.getItem("お")}></img>
-//           <p id="galT">お</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// react-router-domのインポートを追加
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { あ } from "./components/あ";
-import { い } from "./components/い";
-import { う } from "./components/う";
-import { え } from "./components/え";
-import { お } from "./components/お";
+import { fabric } from "fabric";
+import { useState } from "react";
+import { useEffect, useRef } from "react";
+import "./App.css";
+let canvas = undefined;
 
 function App() {
+  const [characterIndex, setCharacterIndex] = useState(0);
+  const canvasRef = useRef(null);
+  const characters = [
+    { key: 0, character: "あ" },
+    { key: 1, character: "い" },
+    { key: 2, character: "う" },
+    { key: 3, character: "え" },
+    { key: 4, character: "お" },
+    { key: 5, character: "か" },
+    { key: 6, character: "き" },
+    { key: 7, character: "く" },
+    { key: 8, character: "け" },
+    { key: 9, character: "こ" },
+    { key: 10, character: "さ" },
+    { key: 11, character: "し" },
+    { key: 12, character: "す" },
+    { key: 13, character: "せ" },
+    { key: 14, character: "そ" },
+    { key: 15, character: "た" },
+    { key: 16, character: "ち" },
+    { key: 17, character: "つ" },
+    { key: 18, character: "て" },
+    { key: 19, character: "と" },
+    { key: 20, character: "な" },
+    { key: 21, character: "に" },
+    { key: 22, character: "ぬ" },
+    { key: 23, character: "ね" },
+    { key: 24, character: "の" },
+    { key: 25, character: "は" },
+    { key: 26, character: "ひ" },
+    { key: 27, character: "ふ" },
+    { key: 28, character: "へ" },
+    { key: 29, character: "ほ" },
+    { key: 30, character: "ま" },
+    { key: 31, character: "み" },
+    { key: 32, character: "む" },
+    { key: 33, character: "め" },
+    { key: 34, character: "も" },
+    { key: 35, character: "や" },
+    { key: 36, character: "ゆ" },
+    { key: 37, character: "よ" },
+    { key: 38, character: "ら" },
+    { key: 39, character: "り" },
+    { key: 40, character: "る" },
+    { key: 41, character: "れ" },
+    { key: 42, character: "ろ" },
+    { key: 43, character: "わ" },
+    { key: 44, character: "を" },
+    { key: 45, character: "ん" },
+  ];
+
+  const setBrush = (canvas) => {
+    if (canvas.freeDrawingBrush) {
+      const brush = canvas.freeDrawingBrush;
+      brush.color = "black";
+      brush.width = 6;
+    }
+  };
+
+  const setBackgroundImage = (canvas) => {
+    fabric.Image.fromURL("", (img) => {
+      img.set({
+        opacity: 1, // 透明度
+        scaleX: canvas.width / img.width, // X軸の拡大率
+        scaleY: canvas.height / img.height, // Y軸の拡大率
+      });
+      canvas.setBackgroundImage(img, canvas.requestRenderAll.bind(canvas)); // 画像を背景に設定
+    });
+  };
+
+  // });
+  useEffect(() => {
+    canvas = new fabric.Canvas(canvasRef.current, {
+      isDrawingMode: true,
+      height: 200,
+      width: 200,
+    });
+    setBrush(canvas);
+    setBackgroundImage(canvas);
+
+    return () => {
+      if (canvas) {
+        canvas.dispose();
+        canvas = undefined;
+      }
+    };
+  }, []);
+
+  const download = (e) => {
+    const link = document.getElementById("hiddenLink");
+
+    link.href = canvasRef.current.toDataURL("image/png");
+    localStorage.setItem(characters[characterIndex].character, link.href);
+    if (characters[characterIndex].character === "ん") {
+      setCharacterIndex((prev) => 0);
+      canvas.clear();
+    } else {
+      setCharacterIndex((prev) => prev + 1);
+      canvas.clear();
+    }
+  };
+
+  const clearCanvas = (e) => {
+    canvas.clear();
+  };
+
+  const NextButtons = () => {
+    if (characters[characterIndex].character === "あ") {
+      return (
+        <div className="next" align="center">
+          <button
+            className="a"
+            onClick={() => {
+              setCharacterIndex((prev) => prev + 1);
+            }}
+          >
+            {characters[characterIndex + 1].character}　＞＞
+          </button>
+        </div>
+      );
+    } else if (characters[characterIndex].character === "ん") {
+      return (
+        <div className="next" align="center">
+          <button
+            classam="a"
+            onClick={() => {
+              setCharacterIndex((prev) => prev - 1);
+            }}
+          >
+            ＜＜　{characters[characterIndex - 1].character}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="next" align="center">
+          <button
+            className="a"
+            onClick={() => {
+              setCharacterIndex((prev) => prev - 1);
+            }}
+          >
+            ＜＜　{characters[characterIndex - 1].character}
+          </button>
+          <button
+            className="a"
+            onClick={() => {
+              setCharacterIndex((prev) => prev + 1);
+            }}
+          >
+            {characters[characterIndex + 1].character}　＞＞
+          </button>
+        </div>
+      );
+    }
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          {/* exactをつけると完全一致になります。Homeはexactをつけてあげます */}
-          <Route exact path="/">
-            <あ />
-          </Route>
-          <Route path="/い">
-            <い />
-          </Route>
-          <Route path="/う">
-            <う />
-          </Route>
-          <Route path="/え">
-            <え />
-          </Route>
-          <Route path="/お">
-            <お />
-          </Route>
-        </Switch>
+    <div className="items" align="center">
+      <div className="canvasDiv">
+        <canvas ref={canvasRef}></canvas>
       </div>
-    </BrowserRouter>
+      <div>
+        <button className="a" onClick={clearCanvas}>
+          クリア
+        </button>
+        <button className="a" onClick={download}>
+          "{characters[characterIndex].character}"の追加
+        </button>
+        <a id="hiddenLink" className="hiddenLink" download="canvas.png">
+          link
+        </a>
+      </div>
+      <ul className="horizontal_scroll">
+        {characters.map((props) => {
+          return (
+            <div
+              className="gal"
+              key={props.key}
+              onClick={() => {
+                setCharacterIndex((prev) => props.key);
+              }}
+            >
+              <img
+                className="galI"
+                src={localStorage.getItem(props.character)}
+              ></img>
+              <p className="galT">{props.character}</p>
+            </div>
+          );
+        })}
+      </ul>
+
+      <NextButtons />
+      <div>
+        <button className="nextKeyboard">キーボードへ</button>
+      </div>
+    </div>
   );
 }
-
 export default App;
